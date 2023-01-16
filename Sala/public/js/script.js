8006138acd05f980c2d08498a5ea3e840c578a39
Murilll <murilosertorio25@gmail.com>
2023-01-16 19:39:47 -0300

    ._...

--- Sala/public/js/script.js
+++ Sala/public/js/script.js
@@ -2,4 +2,6 @@ function headerBosch(){
     document.getElementById("headerBosch").style.height = "1vh";
     document.getElementById("body").style.overflow = "scroll";
     // document.getElementById("header").style.transition = "1s";
+    document.getElementById("escritaHeader").style.display = "none";
+    document.getElementById("escritaHeader").style.transition = "5s";
 }
